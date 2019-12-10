d2c3db7895a59f99dc4e09887f2294ed127c174c
James Hooper <jameshooperdesigns@gmail.com>
2019-12-09 22:42:09 -0700

    Delete comments in the main.js

--- main.js
+++ main.js
@@ -7,9 +7,7 @@ let mm = String(today.getMonth() + 1).padStart(2, '0');
 let yyyy = today.getFullYear().toString().substr(-2);
 
 
-
-// Get current Time
-// let time = today.getHours() + ":" + today.getMinutes();
+// Get current time
 let hours = today.getHours();
 let minutes = today.getMinutes();
 let ampm = hours >= 12 ? 'PM' : 'AM';
@@ -21,10 +19,10 @@ if (minutes < 10) {
     minutes = "0" + minutes;
 }
 
-document.getElementById("currentTime").innerHTML = `<span style="margin-left:38%;">${hours} : ${minutes}</span> <span style="font-size:35%;font-weight:bold;margin-top:90px;">${ampm}</span>`;
+document.getElementById("currentTime").innerHTML = `<span style="margin-left:38%;">${hours}:${minutes}</span> <span style="font-size:35%;font-weight:bold;margin-top:90px;">${ampm}</span>`;
 
 
-// Assign Months to a variable named month
+// Assign months to a variable named month
 const month = ['January','February','March', 'April','May','June','July', 'August','September','October','November','December']
 document.getElementById("spellOutDate").innerHTML = month[today.getMonth()];
 const day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
